caa885cbe15d8b7e8caddd5e962f43cadd6e757f
Jose Luis Gonzalez Rodriguez <jlgr1994@gmail.com>
2023-06-14 23:25:53 -0500

    Commit initial two

---
+++ Images/Images/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Images.Views;
+
+namespace Images;
+
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new DashboardView();
+	}
+}
---
+++ Images/Images/Models/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Images.Models
+{
+    internal class Profile
+    {
+        public string ProfileImage { get; set; }
+        
+        public string Name { get; set; }
+       public string NoPhotos { get; set; }
+    }
+}
